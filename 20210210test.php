16d3d32263da6453a4e39a4cf5be06e74f189a8b
190241-yse <190241th@yse-c.net>
2021-02-10 10:19:56 +0900

    commit

--- 20210210test.php
+++ 20210210test.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>テスト</title>
-</head>
-<body>
-    <h1>
-        テストテスト
-    </h1>
-    <p>
-        testtest
-    </p>
-</body>
-</html>
+<?php
+require_once 'connect.php';
+$items = [
+[
+':code' => 'S00001',
+':name' => 'コーラ',
+':price' => 150,
+':amount' => 500
+],
+[
+':code' => 'T00001',
+':name' => 'お茶',
+':price' => 110,
+':amount' => 800
+],
+[
+':code' => 'T00002',
+':name' => '紅茶',
+':price' => 130,
+':amount' => 300
+],
+];
+$sql = "INSERT INTO items (code, name, price, amount, created_at) ";
+$sql.= "VALUES (:code, :name, :price, :amount ,now())";
+foreach ($items as $item) {
+$prepare = $pdo->prepare($sql);
+$results = $prepare->execute($item);
+if ($prepare->execute($item)) {
+echo '成功';
+} else {
+echo '失敗';
+}
+}
